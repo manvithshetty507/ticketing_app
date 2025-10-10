75fb99f3fb0cd9ae5caad919260fc2dd8ca3f2d0
Manvith Shetty <manvithshetty18@gmail.com>
2025-10-10 12:42:20 +0530

    fixed tests in payments service

--- payments/src/__mocks__/stripe.ts
+++ payments/src/__mocks__/stripe.ts
@@ -2,5 +2,8 @@
 export const stripe = {
     paymentIntents: {
         create: jest.fn().mockResolvedValue({id: 'test_id'})  
+    },
+    paymentMethods: {
+        create: jest.fn().mockResolvedValue({id: 'test_id'})  
     }
 };
